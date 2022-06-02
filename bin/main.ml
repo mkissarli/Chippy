5af4269073f0610fcad4d20261f981dded4c641a
mkissarli <kissarlim@gmail.com>
2022-06-02 22:32:31 +0100

    Basic drawing...maybe

--- bin/main.ml
+++ bin/main.ml
@@ -3,19 +3,20 @@ open Hex
 (* open Sdl *)
 
 module CPU = struct
-  let displayWidth = 64
-  let displayHeight = 48
+  let display_width = 64
+  let display_height = 48
+  let pixel_size = 16
   (* let ramSize = 4096 *)
 
   type display = int array array
 
   type cpu = {
     ram: string;
-    regs: int array;
+    mutable regs: int array;
     dt: int;
     st: int;
     i: int;
-    display: display;
+    mutable display: display;
     pc: int;
   }
 
@@ -27,22 +28,52 @@ module CPU = struct
       dt = 0;
       st = 0;
       i = 0;
-      display = Array.make_matrix ~dimx:displayWidth ~dimy:displayHeight 0;
+      display = Array.make_matrix ~dimx:display_width ~dimy:display_height 0;
       pc = 0;
     }
 
   let inc cpu = { cpu with pc = cpu.pc + 2 }
   let replace l pos v = Array.mapi l ~f:(fun i x -> if i = pos then v else x;)
   let hex_to_int ss = int_of_string ("0x" ^ (String.of_char_list ss))
+  let int_to_bArr i =
+    let rec int_to_bit acc i =
+      if i=0 then acc
+      else int_to_bit (i land 1::acc) (i lsr 1)
+    in
+    let l=int_to_bit [] i in
+      Array.of_list  l
+
+  let int_to_byte n =
+    let b_arr = int_to_bArr n in
+    let pad_number = 8 - (Array.length b_arr) in
+    Array.append (Array.create ~len:pad_number 0) b_arr;;
+
 
   (* OP CODES *)
   module OpCode = struct
-    let cls cpu = { (inc cpu;) with display = Array.make_matrix ~dimx:displayWidth ~dimy:displayHeight 0; }
+    let cls cpu = { (inc cpu;) with display = Array.make_matrix ~dimx:display_width ~dimy:display_height 0; }
     let jp cpu addr = { cpu with pc = addr }
     let set cpu reg value = { cpu with regs = replace cpu.regs reg value }
     let add cpu reg value = { cpu with regs = replace cpu.regs reg (cpu.regs.(reg) + value) }
     let ld_1 cpu addr = { cpu with i = addr }
-    let drw cpu _vx _vy _nibble = cpu
+    let drw cpu vx vy n =
+      let x = cpu.regs.(vx) mod 64 and
+      y = cpu.regs.(vy) mod 32 and
+      sprite = String.sub cpu.ram ~pos:cpu.i ~len:(n * 2) in
+
+      for i = 0 to String.length sprite do
+        let row = int_to_byte (int_of_string (String.sub sprite ~pos:(i * 2) ~len:2)) in
+          for j = 0 to 8 do
+            if row.(j) = 1 then
+              begin
+                if cpu.display.(x + i).(y + j) = 1 then cpu.regs.(15) <- 1 else ();
+                cpu.display.(x + i).(y + j) <- (if cpu.display.(x + i).(y + j) = 0 then 1 else 0);
+                ();
+              end;
+          done;
+      done;
+
+      cpu
   end
 
   let do_op cpu nibble =
@@ -73,21 +104,20 @@ module Graphics = struct
   let create_window width height _title =
     Sdl.init [`VIDEO];
     Sdlrender.create_window_and_renderer ~width ~height ~flags:[]
+
+  let render_pixel renderer x y =
+    Sdlrender.fill_rect renderer (Sdlrect.make4 ~x ~y ~w:CPU.pixel_size ~h:CPU.pixel_size);
 end
 
 let c = CPU.take_nibble (CPU.new_cpu "./IBMLogo.ch8")
 
 let () =
   print_endline c.ram;
-  let width, height = (640, 480) in
-  Random.self_init ();
   Sdl.init [`VIDEO];
-  let window, renderer =
-    Sdlrender.create_window_and_renderer ~width ~height ~flags:[]
-  in
+  let window, renderer = Graphics.create_window (CPU.display_width * CPU.pixel_size) (CPU.display_height * CPU.pixel_size) "Title" in
   for i = 1 to 256 do
     Sdlrender.set_draw_color3 renderer ~b:255 ~g:0 ~r:0 ~a:255;
-    Sdlrender.fill_rect renderer (Sdlrect.make4 ~x:0 ~y:0 ~w:200 ~h:200);
+    Graphics.render_pixel renderer 0 0;
     Sdlrender.render_present renderer;
     Sdltimer.delay ~ms:(1000/60);
   done;
