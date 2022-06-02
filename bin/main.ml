29252698029b93c3376019d6c24fb8c1371db190
mkissarli <kissarlim@gmail.com>
2022-06-02 01:43:23 +0100

    Gitignore and working hex dump

--- bin/main.ml
+++ bin/main.ml
@@ -1,39 +1,43 @@
 open Core
+open Hex
 
 module CPU = struct
   let displayWidth = 64
   let displayHeight = 48
-  let ramSize = 4096
+  (* let ramSize = 4096 *)
 
-  (* type display = int array array *)
-
-  (* type rom = { *)
-  (*   buffer: (int array) array; *)
-  (*   size: int; *)
-  (* } *)
+  type display = int array array
 
   type cpu = {
     mutable ram: string;
     mutable dt: int;
     mutable st: int;
-    mutable display: int;
+    mutable display: display;
     mutable pc: int;
-    (* rom: rom; *)
   }
 
-  let newCpu filename : cpu =
+  let new_cpu filename : cpu =
     let file = In_channel.read_all filename in
     {
-      ram = file;
+      ram = show (Hex.of_string file);
       dt = 0;
       st = 0;
-      display = 0;
-        (* Array.make_matrix displayWidth, displayHeight, 0 ; *)
+      display = Array.make_matrix ~dimx:displayWidth ~dimy:displayHeight 0;
       pc = 0;
-      (* rom = {buffer = Array.make_matrix 1 1 0; size = 1;}; *)
     }
+
+  let take_nibble cpu =
+    if String.length cpu.ram >= 4 then
+      let nibble = String.sub cpu.ram ~pos:0 ~len:4 in
+      print_endline ("Nibble: " ^ nibble);
+
+      { cpu with ram = String.sub cpu.ram ~pos:4 ~len:((String.length cpu.ram) - 4) }
+    else
+      cpu
 end
 
-let c = CPU.newCpu("./IBMLogo.ch8")
-let () = print_endline "test"; print_endline c.ram;
-(* print_endline c.dt; print_endline c.st; print_endline c.display; print_endline c.pc; *)
+let c = CPU.new_cpu("./IBMLogo.ch8")
+let () =
+  print_endline "test";
+  print_endline c.ram;
+  print_endline (CPU.take_nibble c).ram;
